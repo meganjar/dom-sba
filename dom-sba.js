b7eeca3a361eb0955facb64b261ef98721000df3
meganjar <68120966+meganjar@users.noreply.github.com>
2024-12-18 21:29:20 -0700

    chooseTea function added includes eventlistener, and class toggler

--- dom-sba.js
+++ dom-sba.js
@@ -1,34 +1,48 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+// // Set the date we're counting down to
+// var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// // Update the count down every 1 second
+// var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+//   // Get today's date and time
+//   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+//   // Display the result in the element with id="demo"
+//   document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "Your tea is ready! :)";
-  }
-}, 1000);
+//   // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("timer").innerHTML = "Your tea is ready! :)";
+//   }
+// }, 1000);
 
-TeaArr = [
-    {type: green, time: 2 , temp: 160},
-    {type: white, time: 3 , temp: 180},
-    {type: black, time: 4 , temp: 212},
-    {type: herbal, time: 5 , temp: 212} ]
+// TeaArr = [
+//     {type: green, time: 2 , temp: 160},
+//     {type: white, time: 3 , temp: 180},
+//     {type: black, time: 4 , temp: 212},
+//     {type: herbal, time: 5 , temp: 212} ]
+let chooseTeaEl = document.getElementById("choose-tea-btn")
+let dropdownEl = document.getElementById("dropdown-menu")
+
+console.log(chooseTeaEl)
+console.log(dropdownEl)
+    function chooseTea() {
+        chooseTeaEl.addEventListener("click", () => {
+          
+            dropdownEl.classList.toggle("display")
+        })
+        
+    }
+
+    chooseTea()
